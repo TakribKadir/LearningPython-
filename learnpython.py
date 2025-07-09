fdd123de8ed9bc6bd817d8422e9608c1411bcfb6
TakribKadir <mdtakribulkadir@gmail.com>
2025-07-08 22:35:04 -0600

    Update learnpython.py

--- learnpython.py
+++ learnpython.py
@@ -1,4 +1,4 @@
-#  creating Veriable and assigning values*/
+#  Creating Veriable and Assigning values
 name = "Takrib Kadir"
 age = "don't like to share"
 height = 5.6
@@ -7,10 +7,19 @@ print(name);
 print(age);
 print(height);
 print(is_student);
+
 # Task 1. Create variables for your city (string), number of kids (int), and has_pet (boolean)
+
 City = "Vancouver"
 num_kid = 0
 has_pet = False
 print(City);
 print(num_kid);
 print(has_pet);
+
+#Task 2. Change the value of your city variable to another city and print it
+
+age = 36 # Happy Birthday
+print("\nNew age after birthday:", age)
+
+# Task 3. 
