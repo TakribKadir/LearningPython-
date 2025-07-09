eeaa118e9222a9d85c89ebd6a0734ea36e72cbca
TakribKadir <mdtakribulkadir@gmail.com>
2025-07-08 22:23:37 -0600

    Learning Python

---
+++ learnpython.py
@@ -0,0 +1,16 @@
+#  creating Veriable and assigning values*/
+name = "Takrib Kadir"
+age = "don't like to share"
+height = 5.6
+is_student = True
+print(name);
+print(age);
+print(height);
+print(is_student);
+# Task 1.
+City = "Vancouver"
+num_kid = 0
+has_pet = False
+print(City);
+print(num_kid);
+print(has_pet);
