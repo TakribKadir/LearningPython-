5562ac86baa24293714ad862abe94b2d629c8e03
TakribKadir <mdtakribulkadir@gmail.com>
2025-07-10 21:53:45 -0600

    Learning List

---
+++ Learning List.py
@@ -0,0 +1,15 @@
+# Creating a List
+fruits = ["apple", "banana", "cherry", "mango"]
+print("Initial List:", fruits)
+
+#Task 1: Add "orange" to the list and print the updated list
+fruits.append("orange")
+print ("After adding 'orange':", fruits)
+
+#Accessing List Elements
+print ("first fruit:", fruits[0])
+print ("Last fruit:", fruits[-1])
+
+# Task 2: Print the second and third fruits using slicing
+print ("second and third fruits:", fruits[1:3])
+
