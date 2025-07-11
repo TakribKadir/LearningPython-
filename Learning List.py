a1422e0f79bcd9a55325364b0cfd200b3d579905
TakribKadir <mdtakribulkadir@gmail.com>
2025-07-10 22:58:41 -0600

    Update Learning List.py

--- Learning List.py
+++ Learning List.py
@@ -34,4 +34,49 @@ fruits.insert(0, "lemon")
 print ("After adding 'lemon' at beginning:", fruits)
 
 # Removing Items
+# Removes first occurrence of "apple"
+fruits.remove("apple")
+# Removes last item
+popped_fruits= fruits.pop()
+print("After removing 'apple' and popping last item:", fruits)
+print("Popped fruit:", popped_fruits)
 
+#Task 5: Remove "mango" and print the list
+fruits.remove("mango")
+print("After removing 'mango':", fruits)
+
+# Sorting and Reversing
+fruits.sort()
+print("sorted list", fruits)
+fruits.reverse()
+print("reversed list:", fruits)
+
+# Task 6: Sort in reverse alphabetical order without changing original list
+
+sorted_reversed = sorted(fruits, reverse=True)
+print("Reverse-sorted (new list):", sorted_reversed)
+print("Original list remains:", fruits)
+
+# Iterating Over a List
+print("Fruits one by one:" )
+for fruit in fruits:
+    print("Fruit:", fruit)
+
+# Task 7: Print each fruit in uppercase
+print("Fruits in uppercase:")
+for fruits in fruits:
+    print(fruits.upper())
+
+# List Comprehension
+fruits_length = [len(fruits) for fruits in fruits]
+print("Lengths of each fruit name:", fruits_length)
+
+# Task 8: New list with fruits containing the letter 'e'
+fruits_with_e = [fruit for fruit in fruits if 'e' in fruit]
+print("Fruits with 'e':", fruits_with_e)
+
+
+# Given a list of prices, write a function to apply a discount
+prices = [20, 30, 50, 100, 80]
+ #def discounted = (prices, )
+#print(discounted)
